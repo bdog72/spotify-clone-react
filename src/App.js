0b891271903157abd87b6b1b4c53d8ac0d1c23d9
Brian Bycynski <high5bri@hotmail.com>
2020-09-17 01:41:47 -0400

    switching projects - Spotify Clone

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 //
 //
+
 import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className='app'>
-      <h1>Bozo Boy</h1>
+      <h1>Bozo</h1>
     </div>
   );
 }
 
 export default App;
+
+// 7bcf32be284e4d4c8ce457651e6743f5
+// 7bcf32be284e4d4c8ce457651e6743f5
+
+
